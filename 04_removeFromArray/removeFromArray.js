f4d86e3fee7ed9f2e545d929604f2231cd9682da
Kode Whitworth <kodewhitworth01@gmail.com>
2025-03-09 16:48:13 -0400

    finished first set of exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...values) {
+    let modifiedList = [];
+    for (let i = 0; i < list.length; i++) {
+        let valFound = false;
+        for (let j = 0; j < values.length; j++) {
+            if (list[i] === values[j]) {
+                valFound = true;
+                break;
+            }
+        }
+        if (!valFound) {
+            modifiedList.push(list[i]);
+        }
+    }
+    return modifiedList; 
 };
 
 // Do not edit below this line
